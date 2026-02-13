fccb352914b4013365beac198cec7f7bca60a513
Prajwal-G-R-git <prajwalgr01@gmail.com>
2026-02-13 19:07:10 +0530

    Pattern 1

---
+++ p1.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std ; 
+int main () {
+    for(int i = 0 ; i<4 ; i++){ // Rows
+        for(int i = 0 ; i<4 ; i++){ //Columns 
+            cout<<"* ";
+        }
+        cout<<endl;
+        
+
+    }
+}
